7de367bf66e7a57ab6708db3e6e43d06a9ea0b8d
Raccoon103 <zthermau5@gmail.com>
2025-11-03 20:55:58 -0800

    fix default config

--- generation/daps.py
+++ generation/daps.py
@@ -345,7 +345,7 @@ class PDESolverDAPS(PDESolver):
                 hidden_channels=64,
                 n_layers=4
             )
-            model_path = f"generation/fno_pad_trained_forward_{config['dataset']}_128_new_400.pth"
+            model_path = f"generation/fno_pad_trained_forward_{config['dataset']}_128_400.pth"
         elif surrogate_type.lower() == "fno_pad_scarce":
             self.surrogate = FNO_pad(
                 n_modes=(64, 64),
